d0057b309e4b48943b529d7032f826ea4fbfdaa0
taichunmin <taichunmin@gmail.com>
2025-08-24 23:06:24 +0800

    add nstr for calc

--- line/handler/replyCalc.js
+++ line/handler/replyCalc.js
@@ -1,15 +1,21 @@
 const _ = require('lodash')
+const { default: nstr } = require('nstr')
+const { errToJson } = require('../../libs/helper')
 const msgText = require('../msg/text')
 
 module.exports = async (ctx, next) => {
   const text = ctx?.event?.message?.text
   try {
-    if (!_.isString(text)) throw new Error('Not a string')
-    if (/[^0-9!.()*/&%^+<>|~A-Fa-fnoOxX -]/.test(text)) throw new Error('Not a arithmetic string')
+    if (!_.isString(text) || /[^0-9!.()*/&%^+<>|~A-Fa-fnoOxX -]/.test(text)) return await next() // Not a arithmetic string
     const result = new Function(`return ${text}`)() // eslint-disable-line no-new-func
-    await ctx.replyMessage(msgText(`${result}`))
+    const str1 = result.toString()
+    const str2 = nstr(result)
+    await ctx.replyMessage([
+      msgText(str1),
+      ...(str1 === str2 ? [] : [msgText(str2)]),
+    ])
   } catch (err) {
-    // console.log(err)
+    console.log(errToJson(err))
     return await next()
   }
 }
